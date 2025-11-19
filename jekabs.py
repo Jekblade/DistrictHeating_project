070d6850b553c0db7fa55b6b3ef1dbd4f7a81e68
Jekabs Janovs <jekabsjan@gmail.com>
2025-11-19 22:27:15 +0100

    Waste heat app added!!!!

--- jekabs.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import dhnx
-import pandas as pd
-import oemof.solph
-from pyomo.environ import SolverFactory
-
-# Initialize thermal network
-network = dhnx.network.ThermalNetwork()
